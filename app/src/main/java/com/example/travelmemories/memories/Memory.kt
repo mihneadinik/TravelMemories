d1b8cd0cdeb685f7400f8caabc3fd5dcf90190c9
mihneadinik <mihneadinica@gmail.com>
2023-05-22 01:04:17 +0300

    addMemory updates

--- app/src/main/java/com/example/travelmemories/memories/Memory.kt
+++ app/src/main/java/com/example/travelmemories/memories/Memory.kt
@@ -1,3 +1,4 @@
 package com.example.travelmemories.memories
 
+
 data class Memory(var place_name: String, var place_location: String)
--- app/src/main/java/com/example/travelmemories/ui/addmemory/AddMemoryFragment.kt
+++ app/src/main/java/com/example/travelmemories/ui/addmemory/AddMemoryFragment.kt
@@ -8,10 +8,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.travelmemories.R
 import com.example.travelmemories.databinding.FragmentAddMemoryBinding
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.Autocomplete
@@ -23,6 +27,8 @@ import java.util.Locale
 
 class AddMemoryFragment : Fragment() {
     private lateinit var binding: FragmentAddMemoryBinding
+    var coordinates: LatLng? = null
+    var travelType: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +39,29 @@ class AddMemoryFragment : Fragment() {
         binding.lifecycleOwner = this
 
         // set up date picker
+        setDatePicker()
+
+        // set up places autocomplete
+        setPlacesAutocomplete()
+
+        // set up spinner
+        setSpinner()
+
+        // add labels to slider
+        binding.moodSlider.setLabelFormatter { value: Float ->
+            return@setLabelFormatter getMoodLevel(value)
+        }
+
+        return binding.root
+    }
+
+    private fun updateTravelTime(calendar: Calendar) {
+        val myFormat = "dd/MM/yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.ENGLISH)
+        binding.dateOfTravelInput.text = sdf.format(calendar.time)
+    }
+
+    private fun setDatePicker() {
         val myCalendar = Calendar.getInstance()
         val date = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
             myCalendar.set(Calendar.YEAR, year)
@@ -48,8 +77,9 @@ class AddMemoryFragment : Fragment() {
                 myCalendar.get(Calendar.DAY_OF_MONTH)
             ).show()
         }
+    }
 
-        // set up places autocomplete
+    private fun setPlacesAutocomplete() {
         Places.initialize(requireContext(), getString(R.string.api_key))
         binding.locationInput.setOnClickListener {
             // info to be received
@@ -68,14 +98,28 @@ class AddMemoryFragment : Fragment() {
             // start activity
             startActivityForResult(intent, 100)
         }
-
-        return binding.root
     }
 
-    private fun updateTravelTime(calendar: Calendar) {
-        val myFormat = "dd/MM/yyyy"
-        val sdf = SimpleDateFormat(myFormat, Locale.ENGLISH)
-        binding.dateOfTravelInput.text = sdf.format(calendar.time)
+    private fun setSpinner() {
+        val spinnerAdapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.spinner_travel_types,
+            android.R.layout.simple_spinner_item
+        )
+        binding.typeOfTravelSpinner.adapter = spinnerAdapter
+        binding.typeOfTravelSpinner.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                travelType = parent?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -85,6 +129,7 @@ class AddMemoryFragment : Fragment() {
         if (requestCode == 100 && resultCode == RESULT_OK) {
             val place = Autocomplete.getPlaceFromIntent(data!!)
             binding.locationInput.setText(place.address)
+            coordinates = place.latLng
         }
 
         // errors occurred
@@ -99,5 +144,21 @@ class AddMemoryFragment : Fragment() {
             binding.locationInput.requestFocus()
             binding.locationInput.setOnClickListener(null)
         }
+    }
+
+    fun getMoodLevel(value: Float): String {
+        if (value <= 0.2f) {
+            return "Very unhappy"
+        }
+        if (value <= 0.4f) {
+            return "Unhappy"
+        }
+        if (value <= 0.6f) {
+            return "Neutral"
+        }
+        if (value <= 0.8f) {
+            return "Happy"
+        }
+        return "Very happy"
     }
 }
